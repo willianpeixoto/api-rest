d7c72ad9df12c4312d355682e1dcaa10c4be09d4
willian <willian.ferreira@jointecnologia.com.br>
2019-03-13 22:35:22 -0300

    Criptografia de senhas e validação.

---
+++ src/main/java/br/com/projeto/apirest/util/SenhaUtils.java
@@ -0,0 +1,31 @@
+package br.com.projeto.apirest.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class SenhaUtils {
+	
+	/**
+	 * Gera um hash utilizando o BCrypt.
+	 * 
+	 * @param senha
+	 * @return
+	 */
+	public static String gerarBCrypt(String senha) {
+		if(senha == null) {
+			return null;
+		}
+		return new BCryptPasswordEncoder().encode(senha);
+	}
+	
+	/**
+	 * Verifica se a senha é válida.
+	 * 
+	 * @param senha
+	 * @param senhaEncoded
+	 * @return
+	 */
+	public static Boolean validaSenha(String senha, String senhaEncoded) {
+		return new BCryptPasswordEncoder().matches(senha, senhaEncoded);
+	}
+
+}
